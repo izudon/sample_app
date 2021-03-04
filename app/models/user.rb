f102c0da522baa9d36fda1f6dec20ddec1a2df6d
Masakazu IZUI <iz***@g***.com>
2021-03-04 13:43:30 +0900

    ch.06 complete | Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,24 @@ class User < ApplicationRecord
     self.email = email.downcase
   end
 
+  # name のバリデーション
   validates :name ,
             presence: true,
             length: { maximum:  50 }
 
+  # email のバリデーション
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,
             presence: true,
             length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: true #{ case_sensitive: false }
+
+  # ハッシュされたパスワードを持つという指示
+  has_secure_password
+
+  # password のバリデーション
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
 end
