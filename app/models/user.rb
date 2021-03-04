ee685c74343417c4eaa8f3e5ff6a837290918e83
Masakazu IZUI <iz***@g***.com>
2021-03-04 11:27:14 +0900

    ch.06 ->6.2.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
+# coding: utf-8
 class User < ApplicationRecord
-  validates :name , presence: true, length: { maximum:  50 }
-  validates :email, presence: true, length: { maximum: 255 }
+
+  # 保存前にメールアドレスを強制的に小文字に変換
+  before_save do
+    self.email = email.downcase
+  end
+
+  validates :name ,
+            presence: true,
+            length: { maximum:  50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true #{ case_sensitive: false }
 end
