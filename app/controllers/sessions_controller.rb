c5ea404ada6022767596b519e65473272f561ee1
Masakazu IZUI <iz***@g***.com>
2021-03-08 17:25:13 +0900

    ch.08 -> 8.2.3

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+# coding: utf-8
+class SessionsController < ApplicationController
+
+  # GET /login
+  def new
+  end
+
+  # POST /login
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  # DELETE /logout
+  def destroy
+  end
+  
+end
